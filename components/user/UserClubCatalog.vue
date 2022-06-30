af5edd00a9c57c33353e935761c01cfb64728981
deepikarajan <deepuui@ateamindia.com>
2022-06-30 13:32:10 +0530

    create new popup UI development

--- components/user/UserClubCatalog.vue
+++ components/user/UserClubCatalog.vue
@@ -13,7 +13,7 @@
           <div class="tab-section container-fluid mt-4">
             <div class="d-flex justify-content-between align-item-center">
               <h4 class="tab-head">Club Catalog</h4>
-              <button class="btn create-new-btn">Create New</button>
+              <button type="button" class="btn create-new-btn" data-toggle="modal" data-target="#createNewModal">Create New</button>
             </div>
             <div class="inner-tab-section container-fluid py-3">
               <div class="row m-auto">
@@ -166,6 +166,31 @@
         </section>
 
         <!-- End Club catalog -->
+
+        <!-- Create new modal -->
+
+        <div class="modal fade" id="createNewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Create new modal end -->
+
       </div>
     </div>
   </div>
