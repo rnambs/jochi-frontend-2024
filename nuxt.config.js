227affb765ea1a0ac81a35fd21e1388e580ff18e
deepikarajan <deepuui@ateamindia.com>
2022-06-30 13:03:07 +0530

    Edit meeting UI update

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/image/favicon.png' },
             { rel: 'stylesheet', type: 'text/css', href: "https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" },
-            { rel: 'stylesheet', type: 'text/css', href: "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css" },
+            { rel: 'stylesheet', type: 'text/css', href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" },
             { rel: 'stylesheet', type: 'text/css', href: "https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css" },
             { rel: 'stylesheet', type: 'text/css', href: "https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css" },
             { rel: 'stylesheet', type: 'text/css', href: "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css" },
