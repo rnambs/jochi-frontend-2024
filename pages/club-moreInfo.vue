75e74a14687140e46b06d358c96236a4d0a7499d
RaginiAnanth <92146336+RaginiAnanth@users.noreply.github.com>
2022-07-18 17:34:05 +0530

    sidebar error fix

--- pages/club-moreInfo.vue
+++ pages/club-moreInfo.vue
@@ -24,5 +24,8 @@ export default {
       link: [{ rel: "stylesheet", href: "/css/style01.css" }],
     };
   },
+  mounted() {
+    var user_type = localStorage.getItem("user_type");
+  },
 };
 </script>
