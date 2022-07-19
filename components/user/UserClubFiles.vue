a8c9859f955bcb504f49bb26f85b760f9433a267
Sreejith H <sreejith@ateamsoftsolutions.com>
2022-07-19 17:57:41 +0530

    file upload

--- components/user/UserClubFiles.vue
+++ components/user/UserClubFiles.vue
@@ -44,15 +44,15 @@
         </div>
 
         <section id="club-detail" class=" flex-fill d-flex">
-          <div class="club-section container-fluid mt-2 d-flex flex-fill h-40  custom-overflow">
-            <div class="row">
-              <div class="col-md-8">
-                <div class="inner-club club-files container-fluid p-3">
-                  <div class="info-head container-fluid my-3">
+          <div class="club-section container-fluid mt-2 d-flex custom-overflow">
+            <div class="row my-0">
+              <div class="col-md-8 d-flex h-100">
+                <div class="inner-club club-files container-fluid p-3 d-flex flex-column">
+                  <div class="info-head container-fluid mb-2">
                     <h3 class="color-primary font-semi-bold mb-1">{{ headingName }}</h3>
                   </div>
-                  <div class="image-overflow custom-overflow">
-                    <div class="row info-row container-fluid mt-2 mx-auto">
+                  <div class="custom-overflow d-flex flex-column flex-fill">
+                    <div class="row info-row container-fluid my-0 mx-auto">
                       <div
                         class="col-md-3 col-sm-6"
                         v-for="(list, index) in allList"
@@ -188,7 +188,7 @@
                       </div>
                       <div
                         v-if="allList.length == 0"
-                        class="w-100 text-center py-5"
+                        class="w-100 text-center d-flex align-items-center justify-content-center"
                       >
                         <p class="no-data">No Data</p>
                       </div>
@@ -264,7 +264,7 @@
                   "
                 >
                   <div
-                    class="container-fluid p-4 text-center"
+                    class="container-fluid p-2 text-center"
                     v-if="enableEdit"
                   >
                     <form
@@ -341,8 +341,8 @@
                         </div>
                         <button
                           id="clckPrevent"
-                          type=""
-                          class="btn upload-btn mt-3"
+                          type="file"
+                          class="btn btn-primary py-1 my-3"
                         >
                           Browse for file
                         </button>
