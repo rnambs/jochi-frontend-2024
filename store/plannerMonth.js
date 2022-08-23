653f536cb0d7dc27aa1d4b4075bfd86fef67f080
RaginiAnanth <92146336+RaginiAnanth@users.noreply.github.com>
2022-08-23 15:23:07 +0530

    error message correction and invited peers add and remove fix

--- store/plannerMonth.js
+++ store/plannerMonth.js
@@ -101,7 +101,7 @@ const actions = {
         commit('setErrorType', "error");
       }
       else {
-        commit('setErrorMessage', e.response.data.message);
+        commit('setErrorMessage', e.response.data.error);
         commit('setErrorType', "error");
         commit('setSuccessMessage', "");
         commit('setSuccessType', "");
