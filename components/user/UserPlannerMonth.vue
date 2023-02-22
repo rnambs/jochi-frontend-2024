39abb2cbe23941edc4a6e483818ce3767fc6407c
RaginiAnanth <92146336+RaginiAnanth@users.noreply.github.com>
2023-02-22 20:05:53 +0530

    remove the peer validation message changed

--- components/user/UserPlannerMonth.vue
+++ components/user/UserPlannerMonth.vue
@@ -2441,7 +2441,9 @@
                 Complete Assignment Confirmation
               </h3>
             </div>
-            <div class="modal-body px-3 bold-6">Mark assignment as completed?</div>
+            <div class="modal-body px-3 bold-6">
+              Mark assignment as completed?
+            </div>
             <div class="modal-footer">
               <button
                 type="button"
@@ -2591,7 +2593,9 @@
                 Remove Peer Confirmation
               </h3>
             </div>
-            <div class="modal-body px-4">Remove the peer?</div>
+            <div class="modal-body px-4">
+              Are you sure want to remove the peer?
+            </div>
             <div class="modal-footer">
               <button
                 type="button"
--- components/user/UserPlannerWeek.vue
+++ components/user/UserPlannerWeek.vue
@@ -2345,7 +2345,9 @@
               Complete Assignment Confirmation
             </h3>
           </div>
-          <div class="modal-body px-3 bold-6">Mark assignment as completed?</div>
+          <div class="modal-body px-3 bold-6">
+            Mark assignment as completed?
+          </div>
           <div class="modal-footer">
             <button
               type="button"
@@ -2470,7 +2472,9 @@
               Remove Peer Confirmation
             </h3>
           </div>
-          <div class="modal-body px-4">Remove the peer?</div>
+          <div class="modal-body px-4">
+            Are you sure want to remove the peer?
+          </div>
           <div class="modal-footer">
             <button
               type="button"
