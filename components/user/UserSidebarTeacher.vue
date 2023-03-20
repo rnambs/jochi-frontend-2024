242d9c061c45161fe5b8d578523d9305ab1c02b4
RaginiAnanth <92146336+RaginiAnanth@users.noreply.github.com>
2023-03-20 12:16:49 +0530

    menu name changed to Teams & Clubs

--- components/user/UserSidebarTeacher.vue
+++ components/user/UserSidebarTeacher.vue
@@ -316,7 +316,7 @@
                       </div>
                       <span
                         class="ml-3 color-secondary text-capitalize font-medium"
-                        >Extracurriculars</span
+                        >Teams & Clubs</span
                       >
                       <i class="fas fa-chevron-right color-secondary ms-2"></i>
                     </button>
