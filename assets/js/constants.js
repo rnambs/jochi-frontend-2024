bc356e427c8fe0f04dd046e31d40bad5dd74ad83
RaginiAnanth <92146336+RaginiAnanth@users.noreply.github.com>
2023-07-14 19:40:25 +0530

    encode URI added for redirect URI

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -20,8 +20,8 @@ export const MIXPANEL_TOKEN = 'b9cdd17303381147a3e2c53700e2da94'; //client accou
 // export const MIXPANEL_TOKEN = 'e613a6e1f301b9b34075f3d8358cc205'; // test account
 
 // export const GG4L_REDIRECT_LOGIN_URL = "https://sso.gg4l.com/oauth/auth?response_type=code&client_id=PTRQTIBSJC&redirect_uri="
-export const GG4L_REDIRECT_LOGIN_URL = "https://ed.link/sso/login?client_id=515bbb1e-ba82-455d-8fe1-e70535190a8a&redirect_uri=http%3A%2F%2Flocalhost%3A3101%2Fredirect-login&response_type=code"
-// export const GG4L_REDIRECT_LOGIN_URL = "https://ed.link/sso/login?client_id=515bbb1e-ba82-455d-8fe1-e70535190a8a&" + encodeURI(REDIRECT_LOGIN_URL) + "&response_type=code"
+// export const GG4L_REDIRECT_LOGIN_URL = "https://ed.link/sso/login?client_id=515bbb1e-ba82-455d-8fe1-e70535190a8a&redirect_uri=http%3A%2F%2Flocalhost%3A3101%2Fredirect-login&response_type=code"
+export const GG4L_REDIRECT_LOGIN_URL = "https://ed.link/sso/login?client_id=515bbb1e-ba82-455d-8fe1-e70535190a8a&" + encodeURI(REDIRECT_LOGIN_URL) + "&response_type=code"
 export const GG4L_REDIRECT_URL = "https://sso.gg4l.com/oauth/loginwith/logout?redirect_uri="
 
 // encodeURI
